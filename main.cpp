bfd7d76aa49e864fa79fbe74ee55ba07d499d367
mnikt <mgieron2012@gmail.com>
2017-05-01 16:46:35 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,17 +6,17 @@
 
 /*
 Do zrobienia:
--okno zarz¹dzania dru¿yn¹ w trakcie meczu
--spadek poziomu kondycji w trakcie meczu
--dodanie mo¿liwoœci otrzymania kartki i kontuzji
--dodanie paska pokazuj¹cego szanse obu dru¿yn
--dodanie wp³ywu poziomu linii pomocy na szanse dru¿yn
 
--dodanie formacji
--dodanie wp³ywu umieszczenia zawodnika na nie swojej pozycji
 
 */
 using namespace std;
+
+string num_to_str(int number) {
+	string a = "00";
+	a[1] = number % 10 + 48;
+	a[0] = number / 10 + 48;
+	return a;
+}
 
 class Player {
 	enum Position {
@@ -24,21 +24,28 @@ class Player {
 		striker, midfielder, defender, goalkeeper
 	};
 	string name = "Kowalski";
-	double offensive = 70;
-	double pace = 70;
-	double passing = 70;
-	double shooting = 70;
+	double pace = 80;
+	double passing = 80;
+	double shooting = 80;
 	double condition = 99;
-	double defence = 70;
-	double physicality = 70;
+	double defence = 95;
+	double physicality = 80;
 	int overall = 70;
+	int injury = 0;
 	Position position = goalkeeper;
+	bool no_y_card = true;
+	bool no_r_card = true;
 
 public:
 
 	//Funkcja zwraca imiê pi³karza
 	string get_name() {
 		return name;
+	}
+
+	//Funkcja zwraca pozycjê pi³karza
+	int get_position() {
+		return position;
 	}
 
 	///Funkcja zwraca statystyki
@@ -55,8 +62,10 @@ public:
 			return physicality;
 		if (stat == "shooting")
 			return shooting;
-		if (stat == "offensive")
-			return offensive;
+		if (stat == "overall")
+			return overall;
+		if (stat == "injury")
+			return injury;
 		return 70;
 	}
 
@@ -70,7 +79,7 @@ public:
 			overall = (defence * 8 + pace * 2 + passing + physicality * 2 + shooting) / 14;
 			break;
 		case midfielder:
-			overall = (defence + pace + passing * 8 + physicality + shooting) / 12;
+			overall = (defence + pace + passing * 6 + physicality + shooting) / 10;
 			break;
 		case striker:
 			overall = (defence + pace * 2 + passing + physicality * 3 + shooting * 8) / 15;
@@ -80,7 +89,153 @@ public:
 		}
 	}
 
+	///Funkcja zwraca posiadanie kartki
+	bool get_card(string card) {
+		if (card == "yellow")
+			return no_y_card;
+		if (card == "red")
+			return no_r_card;
+		return false;
+	}
+
+	///Funkcja ustawia kartki
+	void set_card(string card) {
+		if (card == "red")
+		{
+			no_r_card = false;
+			return;
+		}
+		else {
+			if (no_y_card == true)
+				no_y_card = false;
+			else no_r_card = false;
+		}
+	}
+
+	///Funkcja obni¿aj¹ca poziom kondycji w trakcie meczu
+	void condition_match_update() {
+		condition = condition - 0.333333f + ((physicality - 70) / 100);
+	}
+
+	///Funkcja otrzymania kontuzji
+	void get_injury(int days) {
+		injury = days;
+	}
+	
 };
+
+///Zmiana zawodników
+void change(int a, int b, Player *player[17], int &c) {
+	if (player[a]->get_card("red") == false || player[b]->get_card("red") == false)
+	{
+		if ((a > 10 || b > 10) && (a<11||b<11))
+			return;
+	}
+	if (c == 0)
+		return;
+	Player *help;
+	help = player[a];
+	player[a] = player[b];
+	player[b] = help;
+	if ((a > 10 || b > 10) && (a<11 || b<11))
+	c--;
+}
+
+///Funkcja wyœwietlaj¹ca statystyki pi³karzy w ekranie zarz¹dzania dru¿yn¹ i pozwalaj¹ca na dokonanie zmian
+void team_management(sf::RenderWindow &window, Player *player[17], int &clicked, int &change_left) {
+	sf::Font font;
+	if (!font.loadFromFile("arial.ttf"))
+		cout << "Couldn't load font";
+
+	sf::Text text[17][9];
+	sf::Sprite card[17];
+	sf::Texture texture;
+	texture.loadFromFile("grafika/trawa.jpg");
+
+	//Ustawienie informacji o podawanych statystykach 
+	sf::Text inf[9];
+	for (int i = 0; i < 9; i++) {
+		inf[i].setFont(font);
+		inf[i].setColor(sf::Color::White);
+		inf[i].setCharacterSize(10);
+		inf[i].setPosition(i * 30 + 40, 85);
+	}
+	inf[0].setPosition(10, 85);
+	inf[0].setString("Name");
+	inf[1].setString("Pos");
+	inf[2].setString("Con");
+	inf[3].setString("Ovr");
+	inf[4].setString("Pac");
+	inf[5].setString("Sho");
+	inf[6].setString("Pas");
+	inf[7].setString("Def");
+	inf[8].setString("Phs");
+
+	for (int i = 0; i < 17; i++) {
+		for (int j = 0; j < 9; j++) {
+			text[i][j].setFont(font);
+			text[i][j].setColor(sf::Color::White);
+			text[i][j].setCharacterSize(15);
+		}
+		card[i].setPosition(305, i * 30 + 100);
+		if (player[i]->get_card("yellow") == false) {
+			texture.loadFromFile("grafika/¿ó³ta_kartka.jpg");
+			card[i].setTexture(texture);
+		} 
+		if (player[i]->get_card("red") == false) {
+			texture.loadFromFile("grafika/czerwona_kartka.jpg");
+			card[i].setTexture(texture);
+		}
+
+		text[i][0].setString(player[i]->get_name());
+		text[i][0].setPosition(5, i * 30 + 100);
+	//	text[i][1].setString(num_to_str(player[i]->get_position()));
+	//	text[i][1].setPosition(70, i * 30 + 100);
+		text[i][2].setString(num_to_str(player[i]->get_stats("condition")));
+		text[i][2].setPosition(100, i * 30 + 100);
+		text[i][3].setString(num_to_str(player[i]->get_stats("overall")));
+		text[i][3].setPosition(130, i * 30 + 100);
+		text[i][4].setString(num_to_str(player[i]->get_stats("pace")));
+		text[i][4].setPosition(160, i * 30 + 100);
+		text[i][5].setString(num_to_str(player[i]->get_stats("shooting")));
+		text[i][5].setPosition(190, i * 30 + 100);
+		text[i][6].setString(num_to_str(player[i]->get_stats("passing")));
+		text[i][6].setPosition(220, i * 30 + 100);
+		text[i][7].setString(num_to_str(player[i]->get_stats("defence")));
+		text[i][7].setPosition(250, i * 30 + 100);
+		text[i][8].setString(num_to_str(player[i]->get_stats("physicality")));
+		text[i][8].setPosition(280, i * 30 + 100);
+	}
+
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+		int x, y;
+		x = sf::Mouse::getPosition(window).x;
+		y = sf::Mouse::getPosition(window).y;
+
+		if (x < 80 && x>5 && y < 595 && y > 100) {
+			if (clicked == (y - 100) / 30)
+				clicked = -1;
+			else if (clicked == -1)
+			clicked = (y - 100) / 30;
+			else {
+				change(clicked, (y - 100) / 30, player, change_left);
+					clicked = -1;
+			}
+		}
+	}
+
+	if (clicked != -1)
+		text[clicked][0].setColor(sf::Color::Yellow);
+
+	for (int y = 0; y < 9; y++)
+		window.draw(inf[y]);
+
+	for (int x = 0; x < 17; x++) {
+		window.draw(card[x]);
+		for (int y = 0; y < 9; y++)
+			window.draw(text[x][y]);
+	}
+}
 
 
 void match(Player player1[17], Player player2[17], sf::RenderWindow &window) {
@@ -104,6 +259,14 @@ void match(Player player1[17], Player player2[17], sf::RenderWindow &window) {
 		team1[i] = &player1[i];
 		team2[i] = &player2[i];
 	}
+
+	//Ekran zarz¹dzania dru¿yn¹
+	bool team_manager = false;
+	sf::Sprite team_manager_button;
+	sf::Texture team_manager_texture;
+	team_manager_button.setPosition(800, 500);
+	team_manager_texture.loadFromFile("grafika/zarzadzanie_druzyna.jpg");
+	team_manager_button.setTexture(team_manager_texture);
 
 	//Wyœwietlenie nazwisk pi³karzy
 	sf::Text players_names_1[17];
@@ -226,7 +389,7 @@ void match(Player player1[17], Player player2[17], sf::RenderWindow &window) {
 		}
 	};
 
-	//Event *event;
+	//Event *event[15];
 
 	window.display();
 	window.draw(team1_name);
@@ -243,21 +406,8 @@ void match(Player player1[17], Player player2[17], sf::RenderWindow &window) {
 	srand(time(NULL));
 
 	double team1_defence = 0, team2_defence = 0, team1_offensive = 0, team2_offensive = 0, team1_midle = 0, team2_midle = 0;
-	for (int i = 0; i < 11; i++) {
-		team1[i]->set_overall();
-		team2[i]->set_overall();
-		team1_defence += (team1[i]->get_stats("defence") + team1[i]->get_stats("pace") + team1[i]->get_stats("physicality"))
-			* (team1[i]->get_stats("overall") - 30) / 3 * (team1[i]->get_stats("condition") + 30) / 90;//(((int)((int)(team1[i]->get_stats("condition") / 90) * 10 + team1[i]->get_stats("condition")) % 100 - 60));
-		team2_defence += (team2[i]->get_stats("defence") + team2[i]->get_stats("pace") + team2[i]->get_stats("physicality"))
-			* ((team2[i]->get_stats("overall") - 30) / 3) * (team2[i]->get_stats("condition") + 30) / 90;// (((int)((int)(team2[i]->get_stats("condition") / 90) * 10 + team2[i]->get_stats("condition")) % 100 - 60));
-		team1_offensive += (team1[i]->get_stats("shooting") + team1[i]->get_stats("pace") + team1[i]->get_stats("physicality"))
-			* (team1[i]->get_stats("overall") - 30) / 3 * (team1[i]->get_stats("condition") + 30) / 90;//(((int)((int)(team1[i]->get_stats("condition") / 90) * 10 + team1[i]->get_stats("condition")) % 100 - 60));
-		team2_defence += (team2[i]->get_stats("shooting") + team2[i]->get_stats("pace") + team2[i]->get_stats("physicality"))
-			* (team2[i]->get_stats("overall") - 30) / 3 * (team2[i]->get_stats("condition") + 30) / 90;//(((int)((int)(team2[i]->get_stats("condition") / 90) * 10 + team2[i]->get_stats("condition")) % 100 - 60));
-	}
-
-	team1_offensive = (8100 - team1_offensive / 11 + team2_defence / 11);
-	team2_offensive = (8100 - team2_offensive / 11 + team1_defence / 11) + 1;
+	int team1_power = 0, team2_power = 0;
+	
 	//cout << team1_offensive << endl << team2_offensive << endl;
 	long int random = 0;
 
@@ -267,13 +417,77 @@ void match(Player player1[17], Player player2[17], sf::RenderWindow &window) {
 
 	Event event[15];
 
+	int clicked = -1;
+
+	int change_left = 3;
+
+	///Ustawienie tekstu z iloœci¹ zmian
+	sf::Text changes[2];
+	changes[0].setString("Changes left:");
+	changes[0].setColor(sf::Color::White);
+	changes[0].setPosition(400, 560);
+	changes[0].setCharacterSize(20);
+	changes[0].setFont(font);
+	changes[1].setColor(sf::Color::White);
+	changes[1].setPosition(525, 560);
+	changes[1].setCharacterSize(20);
+	changes[1].setFont(font);
+
 	///Przebieg meczu
 	///Wykonywane w ka¿dej minucie
 
-	for (int i = 1; i < 9100; i++) {
-		clock[0] = i / 1000 + 48;
-		clock[1] = i / 100 % 10 + 48;
+	for (int i = 1; i < 92; i++) {
+		clock[0] = i / 10 + 48;
+		clock[1] = i % 10 + 48;
 		timer.setString(clock);
+
+		string pomocniczy = "3";
+		pomocniczy[0] = change_left + 48;
+		changes[1].setString(pomocniczy);
+		
+		team1_offensive = 0;
+		team1_defence = 0;
+		team1_midle = 0;
+		team2_offensive = 0;
+		team2_defence = 0;
+		team2_midle = 0;
+		team1_power = 0;
+		team2_power = 0;
+
+		for (int i = 0; i < 17; i++) {
+			team1[i]->set_overall();
+			team2[i]->set_overall();
+		}
+
+		for (int i = 0; i < 11; i++) {
+			team1_defence += (team1[i]->get_stats("defence") + team1[i]->get_stats("pace") + team1[i]->get_stats("physicality"))
+				* (team1[i]->get_stats("overall") - 30) / 3 * (team1[i]->get_stats("condition") + 30) / 90 * team1[i]->get_card("red");//(((int)((int)(team1[i]->get_stats("condition") / 90) * 10 + team1[i]->get_stats("condition")) % 100 - 60));
+			team2_defence += (team2[i]->get_stats("defence") + team2[i]->get_stats("pace") + team2[i]->get_stats("physicality"))
+				* (team2[i]->get_stats("overall") - 30) / 3 * (team2[i]->get_stats("condition") + 30) / 90 * team2[i]->get_card("red");// (((int)((int)(team2[i]->get_stats("condition") / 90) * 10 + team2[i]->get_stats("condition")) % 100 - 60));
+			team1_offensive += (team1[i]->get_stats("shooting") + team1[i]->get_stats("pace") + team1[i]->get_stats("physicality"))
+				* (team1[i]->get_stats("overall") - 30) / 3 * (team1[i]->get_stats("condition") + 30) / 90 * team1[i]->get_card("red");//(((int)((int)(team1[i]->get_stats("condition") / 90) * 10 + team1[i]->get_stats("condition")) % 100 - 60));
+			team2_offensive += (team2[i]->get_stats("shooting") + team2[i]->get_stats("pace") + team2[i]->get_stats("physicality"))
+				* (team2[i]->get_stats("overall") - 30) / 3 * (team2[i]->get_stats("condition") + 30) / 90 * team2[i]->get_card("red");//(((int)((int)(team2[i]->get_stats("condition") / 90) * 10 + team2[i]->get_stats("condition")) % 100 - 60));
+			team1_midle += (team1[i]->get_stats("passing") + team1[i]->get_stats("pace") + team1[i]->get_stats("physicality"))
+				* (team1[i]->get_stats("overall") - 30) / 3 * (team1[i]->get_stats("condition") + 30) / 90 * team1[i]->get_card("red");//(((int)((int)(team1[i]->get_stats("condition") / 90) * 10 + team1[i]->get_stats("condition")) % 100 - 60));
+			team2_midle += (team2[i]->get_stats("passing") + team2[i]->get_stats("pace") + team2[i]->get_stats("physicality"))
+				* (team2[i]->get_stats("overall") - 30) / 3 * (team2[i]->get_stats("condition") + 30) / 90 * team2[i]->get_card("red");//(((int)((int)(team2[i]->get_stats("condition") / 90) * 10 + team2[i]->get_stats("condition")) % 100 - 60));
+		}
+		
+		int a = team1_midle; //zmienna pomocnicza 
+		team1_power = (8100 - team1_offensive / 11 + team2_defence / 11);
+		team2_power = (8100 - team2_offensive / 11 + team1_defence / 11) + 1;
+		team1_midle = (int)(team1_midle / team2_midle * 10);
+		team2_midle = (int)(team2_midle / a * 10);
+
+
+		//Pasek pokazuj¹cy szanse dru¿yn
+		sf::RectangleShape chance1(sf::Vector2f(team2_power * 300 / team1_power, 5));
+		chance1.setPosition(58, 10);
+		chance1.setFillColor(sf::Color::Red);
+		sf::RectangleShape chance2(sf::Vector2f(600, 5));
+		chance2.setPosition(58, 10);
+		chance2.setFillColor(sf::Color::Blue);
 
 		window.display();
 		window.clear();
@@ -284,76 +498,168 @@ void match(Player player1[17], Player player2[17], sf::RenderWindow &window) {
 		window.draw(goals1_text);
 		window.draw(goals2_text);
 		window.draw(timer);
-
-		for (int i = 0; i < event_counter; i++) {
-			event[i].draw(window);
+		window.draw(team_manager_button);
+		window.draw(chance2);
+		window.draw(chance1);
+		if (team_manager) {
+			window.draw(changes[0]);
+			window.draw(changes[1]);
 		}
 
-		for (int i = 0; i < 17; i++) {
-			window.draw(players_names_1[i]);
-			window.draw(players_names_2[i]);
+		for (int a = 0; a < event_counter; a++) {
+			event[a].draw(window);
 		}
 
-		random = rand() % 300000000000;
+		if (!team_manager) {
+			for (int b = 0; b < 17; b++) {
+				window.draw(players_names_1[b]);
+				window.draw(players_names_2[b]);
+			}
+		}//OKNO ZARZ¥DZANIA DRU¯YN¥
+		else {
+			team_management(window, team1, clicked, change_left);
+		}
+		
+		int player_power;
+		for (int i = 0; i < 11; i++) {
+			player_power =  ((team1[i]->get_stats("shooting") + team1[i]->get_stats("pace") + team1[i]->get_stats("physicality")) * (team1[i]->get_stats("overall") - 30) / 3 * (team1[i]->get_stats("condition") + 30) / 90);
+		//player_power = 1/(player_power * 11 / team1_offensive);
+			for (int j = 0; j < team1_midle; j++) {
+				if (team1[i]->get_card("red") == false) break;
+				random = rand() % (team1_power * team2_power);
+				if (random % player_power == 0)
+				{
+					goals1++;
+					goal1++;
+					goals1_text.setString(goal1);
+					event[event_counter].set(event_counter, "team1", team1[i]->get_name(), clock, "goal");
+					event_counter++;
+					break;
+				}
+			}
+				 player_power = (team2[i]->get_stats("shooting") + team2[i]->get_stats("pace") + team2[i]->get_stats("physicality"))* (team2[i]->get_stats("overall") - 30) / 3 * (team2[i]->get_stats("condition") + 30) / 90;
+			//	 player_power = 1/(player_power / team2_offensive * 11);
+				for (int j = 0; j < team2_midle; j++) {
+					if (team2[i]->get_card("red") == false) break;
+					random = rand() % (team1_power * team2_power);
+					if (random % player_power == 0)
+					{
+						goals2++;
+						goal2++;
+						goals2_text.setString(goal2);
+						event[event_counter].set(event_counter, "team2", team2[i]->get_name(), clock, "goal");
+						event_counter++;
+						break;
+					}
+				}
+				//Spadek kondycji
+				if (team1[i]->get_card("red"))
+				team1[i]->condition_match_update();
+				if (team2[i]->get_card("red"))
+				team2[i]->condition_match_update();
+		}
 
-		if (random % (int)team1_offensive == 0) {
-			goals1++;
-			goal1++;
-			goals1_text.setString(goal1);
-			event[event_counter].set(event_counter, "team1", team1[0]->get_name(), clock, "goal");
+		//Otrzymanie ¿ó³tej kartki
+		random = rand() % 550;
+		if (random < 11) {
+			if (team1[random]->get_card("yellow") == true) {
+				event[event_counter].set(event_counter, "team1", team1[random]->get_name(), clock, "y_card");
+				event_counter++;
+				team1[random]->set_card("yellow");
+			}else {
+				event[event_counter].set(event_counter, "team1", team1[random]->get_name(), clock, "r_card");
+				event_counter++;
+				team1[random]->set_card("red");
+			}
+		}
+		else if (random < 22) {
+			if (team2[random-11]->get_card("yellow") == true) {
+				event[event_counter].set(event_counter, "team2", team2[random-11]->get_name(), clock, "y_card");
+				event_counter++;
+				team2[random-11]->set_card("yellow");
+			}
+			else {
+				event[event_counter].set(event_counter, "team2", team2[random-11]->get_name(), clock, "r_card");
+				event_counter++;
+				team2[random-11]->set_card("red");
+			}
+		}
+
+		//Otrzymanie czerwonej kartki
+		random = rand() % 5000;
+		if (random < 11) {
+			event[event_counter].set(event_counter, "team1", team1[random]->get_name(), clock, "r_card");
 			event_counter++;
+			team1[random]->set_card("red");
 		}
-
-		if (random % (int)team2_offensive == 0) {
-			goals2++;
-			goal2++;
-			goals2_text.setString(goal2);
-			event[event_counter].set(event_counter, "team2",team2[0]->get_name(), clock, "goal");
+		else if (random < 22) {
+			event[event_counter].set(event_counter, "team2", team2[random - 11]->get_name(), clock, "r_card");
 			event_counter++;
+			team2[random - 11]->set_card("red");
 		}
 
+		//Otrzymanie kontuzji
+		random = rand() % 3500;
+		if (random < 11) {
+			event[event_counter].set(event_counter, "team1", team1[random]->get_name(), clock, "injury");
+			event_counter++;
+			team1[random]->get_injury(rand()%20);
+		}
+		else if (random < 22) {
+			event[event_counter].set(event_counter, "team2", team2[random - 11]->get_name(), clock, "injury");
+			event_counter++;
+			team2[random - 11]->get_injury(rand()%20);
+		}
+
+		sf::Clock clock;
+		sf::Time time;
+		while (time.asSeconds() <= 1.0f) {
+			time += clock.getElapsedTime();
+			clock.restart();
+			window.display();
+			window.clear();
+			window.draw(background);
+			window.draw(events);
+			window.draw(team1_name);
+			window.draw(team2_name);
+			window.draw(goals1_text);
+			window.draw(goals2_text);
+			window.draw(timer);
+			window.draw(team_manager_button);
+			window.draw(chance2);
+			window.draw(chance1);
+			if(team_manager) {
+				window.draw(changes[0]);
+				window.draw(changes[1]);
+			}
+
+			for (int a = 0; a < event_counter; a++) {
+				event[a].draw(window);
+			}
+
+			if (!team_manager) {
+				for (int b = 0; b < 17; b++) {
+					window.draw(players_names_1[b]);
+					window.draw(players_names_2[b]);
+				}
+			}//OKNO ZARZ¥DZANIA DRU¯YN¥
+			else {
+				team_management(window, team1, clicked, change_left);
+			}
+			if (sf::Mouse::getPosition(window).x > 800 && sf::Mouse::getPosition(window).x < 923 && sf::Mouse::getPosition(window).y > 500 && sf::Mouse::getPosition(window).y < 563 && sf::Mouse::isButtonPressed(sf::Mouse::Left))
+				team_manager = !team_manager;
+		}
+		clock.restart();
 
 	}
-/*	if (event_counter == 1)
-		delete event;
-	else if (event_counter > 1)
-	delete[] event;*/
-
-	cout << goals1 << " : " << goals2 << endl;
+	///Wys³anie danych pomeczowych, statystyk itp.
 
 }
-/*
-void match(sf::RenderWindow &window){
-	sf::RectangleShape TeamManager(sf::Vector2f(100, 50));
-	//TeamManager.setTexture();
-	TeamManager.setPosition(10, 10);
-	sf::RectangleShape TimeTable(sf::Vector2f(100, 50));
-	//TimeTable.setTexture();
-	TimeTable.setPosition(10, 70);
-	sf::RectangleShape NextMatch(sf::Vector2f(100, 50));
-	//NextMatch.setTexture();
-	NextMatch.setPosition(10, 130);
-	sf::RectangleShape Exit(sf::Vector2f(100, 50));
-	//Exit.setTexture();
-	Exit.setPosition(10, 190);
-	
-	sf::Mouse::getPosition(window);
-	window.draw(TeamManager);
-	window.draw(TimeTable);
-	window.draw(NextMatch);
-	window.draw(Exit);
-
-}*/
-
-
-
 
 int main() {
-
 	Player team1[17], team2[17];
 	sf::RenderWindow window(sf::VideoMode(1000, 600), "My window");
-	window.setFramerateLimit(1000);
-	
+	window.setFramerateLimit(8);
 	for (int i = 0; i < 10; i++) {
 		match(team1, team2, window);
 		Sleep(1000); 
